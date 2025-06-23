1ea31d1764b72532d6a48d19f470b000934edc68
mastercode <clevereagledev@gmail.com>
2025-06-23 03:26:32 -0700

    Update index.js

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -11,7 +11,7 @@ root.render(
   <ContextProvider>
     <AIContextProvider>
       <FilterContextProvider>
-        <GlobalStyle />
+        <GlobalStyle /> 
         <App />
       </FilterContextProvider>
     </AIContextProvider>
